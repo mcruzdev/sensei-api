8baa34e678100a1f6588cfe072dad66929f01425
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-12-03 20:37:25 -0300

    Cria projeto Spring

---
+++ src/main/java/com/platformoon/sensei/api/SenseiApiApplication.java
@@ -0,0 +1,13 @@
+package com.platformoon.sensei.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenseiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platformoon/sensei/api/SenseiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platformoon.sensei.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenseiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
